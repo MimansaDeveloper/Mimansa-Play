0d90a0b534dc549ec78f1b97ea2927fb68ebe71b
devgupta4526 <devguptamsi@gmail.com>
2024-10-17 14:13:26 +0530

    minor changes

--- frontend/src/pages/HeroSection.jsx
+++ frontend/src/pages/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const HeroSection = () => {
   return (
@@ -6,22 +6,30 @@ const HeroSection = () => {
       {/* Left Section */}
       <div className="flex flex-col w-[50%] h-full ml-20 mt-4">
         <h1 className="font-comic-neue ml-1 font-bold text-[1.94vw] lg:text-[2.50vw] leading-[2.22vw] lg:leading-[3.00vw] text-black mt-[1.85vh] lg:mt-44">
-          Unlock Your Child’s Speech with <br/> Fun, AI-Powered Games
+          Unlock Your Child’s Speech with <br /> Fun, AI-Powered Games
         </h1>
         {/* <p className="font-comic-neue font-normal ml-1 text-2xl leading-[2vw]  text-black mt-5 ">
           Designed by experts from <span className='text-[rgba(38,65,149,1)] font-bold '>IIT & Havard University</span>, loved by kids, <br/>and trusted by parents. Give your child the speech tools they need <br/> with interactive, engaging activities.
         </p> */}
-        <ul className="font-comic-neue font-normal ml-[-1vw] text-2xl leading-[2vw] text-black mt-5 list-none pl-5">
-  <li>Designed by experts from <span className='text-[rgba(38,65,149,1)] font-bold'>IIT & Harvard University</span> Loved by kids</li>
-  <li>and Trusted by parents Give your child the speech tools they need</li>
-  <li>with interactive, engaging activities</li>
-</ul>
+        <ul className=" w-full font-comic-neue font-normal ml-[-1vw] text-[24px] leading-[2vw] text-black mt-5 list-none pl-5">
+          <li>
+            Designed by experts from{" "}
+            <span className="text-[rgba(38,65,149,1)] font-bold">
+              IIT & Havard University
+            </span>
+            , loved by kids,
+          </li>
+          <li>
+            and trusted by parents. Give your child the speech tools they need
+          </li>
+          <li>with interactive, engaging activities.</li>
+        </ul>
 
         {/* <button className="bg-[#DBCDF0] w-fit p-4 lg:p-6 mt-6 lg:mt-10 text-black font-semibold rounded-md hover:bg-[#f4c300] transition duration-300">
           Join the Waitlist for Early Access
         </button> */}
-         {/* Input & Button */}
-         <div className="flex w-full mt-10">
+        {/* Input & Button */}
+        <div className="flex w-full mt-10">
           <input
             type="email"
             placeholder="Enter your email to unlock the early access"
@@ -31,11 +39,18 @@ const HeroSection = () => {
             Sign me up!
           </button>
         </div>
-        <h3 className='ml-1 mt-2 font-light text-md'><span className='text-red-700'>*</span>We’ll only send what’s worth opening – no spam, guaranteed!</h3>
+        <h3 className="ml-1 mt-2 font-light text-md">
+          <span className="text-red-700">*</span>We’ll only send what’s worth
+          opening – no spam, guaranteed!
+        </h3>
       </div>
       {/* Right Section (Image) */}
       <div className="flex justify-center items-center w-[50%]  mt-[-60px]">
-        <img src="/herosection.png" alt="Hero Section" className='w-full p-12 mr-20' />
+        <img
+          src="/herosection.png"
+          alt="Hero Section"
+          className="w-full p-12 mr-20"
+        />
       </div>
     </div>
   );
