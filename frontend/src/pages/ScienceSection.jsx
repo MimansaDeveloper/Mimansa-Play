ba69601a715e09f4fd3878c3ff6b5276daafc5fe
devgupta4526 <devguptamsi@gmail.com>
2024-10-17 17:00:26 +0530

    test

--- frontend/src/pages/ScienceSection.jsx
+++ frontend/src/pages/ScienceSection.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ScienceSection = () => {
   return (
-    <div className="bg-[rgba(250,237,203,1)] h-[110vh] flex flex-col justify-center items-center px-4 md:px-8">
+    <div className="bg-[rgba(250,237,203,1)]  flex flex-col justify-center items-center px-4 md:px-8">
       {/* Header Section */}
-      <div className="text-center mb-12 mt-2">
+      <div className="text-center  mb-12 mt-[6.5rem]">
         <h2 className="font-comic-neue text-[2.5vw] leading-[2vw] font-bold font-comic text-black mb-3">
           The Science Behind Us
         </h2>
@@ -14,9 +14,9 @@ const ScienceSection = () => {
       </div>
 
       {/* Main Content */}
-      <div className="mt-[-2vw] flex flex-col md:flex-row  items-center justify-center gap-12 md:gap-24">
+      <div className="mt-[-3vw] flex flex-col md:flex-row  items-center justify-center gap-12 md:gap-24">
         {/* Image Placeholder */}
-        <div className="w-[35vw] h-[35vw] flex justify-center items-center">
+        <div className="w-[38vw] h-[38vw] flex justify-center items-center">
           <img
             src="/sciencesectionimage.png"
             alt="Science Image"
@@ -25,7 +25,7 @@ const ScienceSection = () => {
         </div>
 
         {/* Text and Button */}
-        <div className="text-left max-w-lg">
+        <div className="text-left max-w-lg mt-2">
           <p className="text-2xl   font-comic text-gray-800 leading-10 mb-8">
             Our games use methods endorsed by leading early childhood development experts and speech therapists to ensure your child gets the best learning experience possible. With AI-driven insights, your child receives personalized support on their journey to better speech.
           </p>
